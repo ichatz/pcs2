77131883fe6a21b1d58779ee05ef623e6f17a493
Ioannis Chatzigiannakis <ichatz@github.com>
2017-10-19 15:13:04 +0200

    introducing bubble sort

---
+++ lecture 5 - sorting/bubble.py
@@ -0,0 +1,24 @@
+def swap(list, i, j):
+    temp = list[i]
+    list[i] = list[j]
+    list[j] = temp
+
+def bubbleSort(list):
+    for j in range(0, len(list) - 1):
+        counter = 0
+        for i in range(0, len(list) - 1):
+            if (list[i] > list[i+1]):
+                counter += 1
+                swap(list, i, i+1)
+
+            print(list)
+
+        print("-------", counter)
+        if (counter == 0):
+            print("break")
+            break
+
+a = [5, 1, 4, 2, 6, 3]
+print("Initial: ", a)
+bubbleSort(a)
+print("Final: ", a)
--- lecture 5 - sorting/selection.py
+++ lecture 5 - sorting/selection.py
@@ -4,16 +4,27 @@ def swap(list, i, j):
     list[j] = temp
 
 def selectionSort(list):
+    counter = 0
     for i in range(0, len(list)):
         min = i
         for j in range(i + 1, len(list)):
+            counter += 1
             if list[j] < list[min]:
                 min = j
 
         swap(list, i, min)
 
+    size = len(list)
+    print("Size: ", size, " comparisons: ", counter, " theory:", size*(size-1)/ 2)
 
 a = [5, 1, 6, 2, 4, 3]
 print(a)
 selectionSort(a)
 print(a)
+
+for i in range(5, 11):
+    temp = []
+    for x in range(0, i):
+        temp.append(x)
+
+    selectionSort(temp)
